44481ced7e531f89cd8667c6e97913fd2e30e3a8
toyohide <hide.toyoda@gmail.com>
2023-10-06 10:07:49 +0900

    eslint設定

---
+++ App.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
